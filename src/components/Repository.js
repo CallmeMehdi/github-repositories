50e093ba85a83d94de24dfb6ddd1c8a1f839a53d
CallmeMehdi <mehdi.mabrouki@yahoo.com>
2021-10-09 22:31:21 +0100

    add: added repo properties

--- src/components/Repository.js
+++ src/components/Repository.js
@@ -7,11 +7,30 @@ import {
   Avatar,
   useColorModeValue,
   Link,
+  Flex,
+  Icon,
 } from "@chakra-ui/react";
 import "../assets/stylesheet.css";
 import "../assets/github-lang-colors.css";
+import { AiFillStar, AiOutlineFork } from "react-icons/ai";
+import { FaBalanceScale } from "react-icons/fa";
 
 export default function Repository(props) {
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
   return (
     <Center
       style={{ transition: "0.4s", cursor: "pointer" }}
@@ -36,20 +55,85 @@ export default function Repository(props) {
             <Text fontSize={"14px"} fontWeight={200}>
               {props.repo.description}
             </Text>
-            {props.repo.language ? (
-              <Text
-                m={4}
-                fontSize={"14px"}
-                fontWeight={400}
-                display="flex"
-                alignItems="center"
-              >
-                <span className={`dot ` + props.repo.language}></span>
-                <span>{props.repo.language}</span>
-              </Text>
+            {props.repo.language || props.repo.stargazers_count ? (
+              <Flex>
+                {props.repo.language ? (
+                  <Text
+                    m={3}
+                    fontSize={"14px"}
+                    fontWeight={400}
+                    display="flex"
+                    alignItems="center"
+                  >
+                    <span className={`dot ` + props.repo.language}></span>
+                    <span>{props.repo.language}</span>
+                  </Text>
+                ) : (
+                  <span></span>
+                )}
+                {props.repo.stargazers_count ? (
+                  <Text
+                    m={3}
+                    fontSize={"14px"}
+                    fontWeight={400}
+                    display="flex"
+                    alignItems="center"
+                  >
+                    <Icon mr={2} as={AiFillStar} />
+                    <span>{props.repo.stargazers_count}</span>
+                  </Text>
+                ) : (
+                  <Text
+                    m={3}
+                    fontSize={"14px"}
+                    fontWeight={400}
+                    display="flex"
+                    alignItems="center"
+                  >
+                    <Icon mr={2} as={AiFillStar} />
+                    <span>0</span>
+                  </Text>
+                )}
+                <Text
+                  m={3}
+                  fontSize={"14px"}
+                  fontWeight={400}
+                  display="flex"
+                  alignItems="center"
+                >
+                  <Icon mr={2} as={AiOutlineFork} />
+                  <span>{props.repo.forks_count}</span>
+                </Text>
+
+                {props.repo.license ? (
+                  <Text
+                    m={3}
+                    fontSize={"14px"}
+                    fontWeight={400}
+                    display="flex"
+                    alignItems="center"
+                  >
+                    <Icon mr={3} as={FaBalanceScale} />
+                    <span>{props.repo.license.name}</span>
+                  </Text>
+                ) : (
+                  <span></span>
+                )}
+
+                <Text
+                  m={3}
+                  fontSize={"14px"}
+                  fontWeight={400}
+                  display="flex"
+                  alignItems="center"
+                >
+                  <span>Last Updated at {new Date(props.repo.updated_at).getDate()} {monthNames[new Date(props.repo.updated_at).getMonth()]}</span>
+                </Text>
+              </Flex>
             ) : (
               <span></span>
             )}
+            {props.repo.stargazers_count ? <span></span> : <span></span>}
           </div>
         </Stack>
       </Box>
