01474c52b1c7851576ff6f00d8c6182ebdbdd702
CallmeMehdi <mehdi.mabrouki@yahoo.com>
2021-10-10 09:20:47 +0100

    add: added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import RepositoryContainer from './components/RepositoryContainer';
 
-test('renders learn react link', () => {
+test('Renders search bar in home', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const searchBar = screen.getByPlaceholderText("Type a")
+  expect(searchBar).toBeInTheDocument();
 });
+
+test('Renders repositories of user CallmeMehdi', () => {
+  const { container } = render(<RepositoryContainer reposUrl="https://api.github.com/users/CallmeMehdi/repos"/>);
+  const currentProject = container.querySelector(".css-ifuklx")  
+  expect(currentProject).not(null)
+
+})
