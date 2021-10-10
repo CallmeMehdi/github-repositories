9b6043d6dd9cf07db4badeb70e9cea66ae1fb157
CallmeMehdi <mehdi.mabrouki@yahoo.com>
2021-10-10 10:50:13 +0100

    add: added repositories filtering

--- src/components/RepositoryContainer.js
+++ src/components/RepositoryContainer.js
@@ -1,4 +1,4 @@
-import { Flex, Stack, useColorModeValue, Box } from "@chakra-ui/react";
+import { Flex, Stack, useColorModeValue, Box, InputGroup, Input } from "@chakra-ui/react";
 import axios from "axios";
 import Repository from "./Repository";
 
@@ -6,15 +6,16 @@ import React, { useEffect } from "react";
 
 export default function RepositoryContainer(props) {
   const [repos, setRepos] = React.useState([]);
+  const [filteredRepos, setFilteredRepos] = React.useState([]);
 
   useEffect(() => {
-
-    if (props.refresh){
-      setRepos([])
-      props.parentCallback()
-    }else{
+    if (props.refresh) {
+      setRepos([]);
+      props.parentCallback();
+    } else {
       getRepos(props.reposUrl);
     }
+    setFilteredRepos(repos)
   }, [props.reposUrl, props.refresh]);
 
   const getRepos = (reposUrl) => {
@@ -31,19 +32,35 @@ export default function RepositoryContainer(props) {
     }
   };
 
+  const filterRepos = (event) => {
+    let regexp = new RegExp('^' + event.target.value)
+
+    console.log(event.target.value)
+    console.log(repos.filter((repo) => repo.name.match(regexp)))
+    setFilteredRepos(repos.filter((repo) => repo.name.match(regexp)))
+  }
+
   return (
     <Flex style={{ height: "90vh" }} py={8} bg="#F5F5F5" mx="auto">
       <Stack
         boxShadow={"2xl"}
         bg={useColorModeValue("white", "gray.700")}
         rounded={"xl"}
-        p={[2,2,10,10]}
+        p={[2, 2, 10, 10]}
         w={["80vw", "80vw", "50vw"]}
         spacing={8}
         overflowY={"auto"}
       >
-        {repos.length ? (
-          repos.map((repo) => <Repository repo={repo} />)
+        <InputGroup size="md" bg="white">
+          <Input
+            pr="4.5rem"
+            type="text"
+            placeholder="Filter repositories by name"
+            onChange={filterRepos}
+          />
+        </InputGroup>
+        {filteredRepos.length ? (
+          filteredRepos.map((repo) => <Repository repo={repo} />)
         ) : (
           <Box display="flex" justifyContent="center">
             There are no repositories found.
